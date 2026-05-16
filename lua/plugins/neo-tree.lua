f3e7238ad341fb71daa3c4afb77eb5405a5468a0
PaulFinch <1887365+PaulFinch@users.noreply.github.com>
2026-05-16 11:47:21 +0200

    Update

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -6,6 +6,11 @@ end
 neo_tree.setup({
     close_if_last_window = true,
     filesystem = {
+        filtered_items = {
+            visible = true,
+            hide_dotfiles = false,
+            hide_gitignored = false,
+        },
         follow_current_file = {
             enabled = true,
         },

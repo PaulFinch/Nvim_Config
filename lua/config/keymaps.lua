a5bb754405bafb38367a8609cbe8f36926156e9f
PaulFinch <1887365+PaulFinch@users.noreply.github.com>
2026-06-06 10:25:02 +0200

    Update

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -34,7 +34,7 @@ vim.keymap.set("n", "<leader>pu", function()
     vim.pack.update()
 end, { desc = "[P]ackage [U]pdate" })
 
--- Yank
+-- Autocmd
 vim.api.nvim_create_autocmd("TextYankPost", {
     desc = "Highlight when yanking (copying) text",
     group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),

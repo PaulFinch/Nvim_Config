fa555f94596748d93fbc3f76681ffa0144b956ef
PaulFinch <1887365+PaulFinch@users.noreply.github.com>
2026-06-08 10:35:16 +0200

    Update

--- lsp/lua_ls.lua
+++ lsp/lua_ls.lua
@@ -2,9 +2,16 @@
 return {
     settings = {
         Lua = {
+            runtime = {
+                version = "LuaJIT",
+            },
             workspace = {
-                library = vim.api.nvim_get_runtime_file('', true),
-            }
+                library = vim.api.nvim_get_runtime_file("", true),
+                checkThirdParty = false,
+            },
+            telemetry = {
+                enable = false,
+            },
         }
     }
 }

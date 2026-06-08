ac3b2a3cf047f30064579a0ed9ca12e6ea4a8ba6
PaulFinch <1887365+PaulFinch@users.noreply.github.com>
2026-06-08 18:01:03 +0200

    Update

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -27,3 +27,10 @@ vim.api.nvim_create_autocmd("FileChangedShellPost", {
 		vim.notify("File reloaded from disk")
 	end,
 })
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "help",
+  callback = function()
+    vim.cmd("wincmd L")
+  end,
+})
